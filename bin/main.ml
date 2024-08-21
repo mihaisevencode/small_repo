6a7a56889a94a0882f1388b56046aac46ce7af9a
mihaisevencode <mihai.miuta@7code.ro>
2024-08-21 16:39:47 +0300

    Add some data

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,15 @@
-let () = print_endline "Hello, World!"
+let limit = 10_000
+
+let get_random_string () =
+  Random.self_init ();
+  Int64.to_string @@ Random.bits64 ()
+;;
+
+let () =
+  let out_channel = open_out "data.txt" in
+  for _i = 0 to limit do
+    let message = get_random_string () in
+    Printf.fprintf out_channel "%s\n" message
+  done;
+  close_out out_channel
+;;
